dcae5c7a28b77c5b3e567786dd13ec845a92cc68
Ditrim <ditrimgnom@gmail.com>
2021-11-01 20:40:32 +0300

    Homework1

--- src/main/java/ru/gb/config/SecurityConfig.java
+++ src/main/java/ru/gb/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ru.gb.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.gb.sevices.UserService;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
